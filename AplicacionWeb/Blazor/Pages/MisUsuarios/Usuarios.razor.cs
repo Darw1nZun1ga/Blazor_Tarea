b1b874f9718b448f5a600a2fa4fcaf19536319f8
Darw1nZun1ga <113458284+Darw1nZun1ga@users.noreply.github.com>
2022-11-19 12:14:52 -0600

    Tarea_Blazor
    Ejercicio #5

---
+++ AplicacionWeb/Blazor/Pages/MisUsuarios/Usuarios.razor.cs
@@ -0,0 +1,19 @@
+﻿using Blazor.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+
+namespace Blazor.Pages.MisUsuarios
+{
+    public partial class Usuarios
+    {
+        //Injeccion de la clase program
+        [Inject] private IUsuarioServicio usuarioServicio { get; set; }
+
+        private IEnumerable<Usuario> lista { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            lista = await usuarioServicio.GetLista();
+        }
+    }
+}
---
+++ AplicacionWeb/Modelos/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos
+{
+    public class Usuario
+    {
+        [Required(ErrorMessage ="Debe ingresar un codigo")]
+        public string Codigo { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un Nombre")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Debe ingresar una clave")]
+        public string Clave { get; set; }
+        public string? Correo { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un Rol")]
+        public string Rol { get; set; }
+        public bool EstaActivo { get; set; }
+    }
+}
